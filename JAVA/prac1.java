9b8d5fef7d78f0c02b2ace78ecefd41b200ec140
mrnithesh <mr.nithesh.k@gmail.com>
2025-01-23 18:48:48 +0530

    java commit 5

--- JAVA/prac1.java
+++ JAVA/prac1.java
@@ -6,16 +6,19 @@ public class prac1 {
         System.out.println("Hello world!");
         System.out.println("Enter a : ");
         int a = S.nextInt();
-        System.out.println("Enter b :");
-        int b = S.nextInt();
+        // System.out.println("Enter b :");
+        // int b = S.nextInt();
         /*b= a+b;
         a=b-a;
         b=b-a;
         System.out.println("Value of a :" + a);
         System.out.println("Value of b :" + b);
         */
-        System.out.println("The area of the rectangle is :" + a*b);
-        System.out.println("The perimeter of the rectangle is :" + 2*(a+b) );
+        // System.out.println("The area of the rectangle is :" + a*b);
+        // System.out.println("The perimeter of the rectangle is :" + 2*(a+b) );
+        float farenheit = (a * 9.0f/5.0f)+32;
+        System.out.println("Farenheit is equal to :"+ farenheit);
+
     }
 
 }
