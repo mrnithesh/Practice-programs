02dae71d83722fda765dbac53c68548ab9181657
mrnithesh <mr.nithesh.k@gmail.com>
2025-01-23 18:32:32 +0530

    java commit 2

--- JAVA/prac1.java
+++ JAVA/prac1.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class prac1 {
     public static void main(String [] args){
+        Scanner S = new Scanner(System.in);
         System.out.println("Hello world!");
+        System.out.println("Enter a number: ");
+        int a = S.nextInt();
+        System.out.println(a*2);
+
     }
 
 }
