818b2888e1f367d21dd39a1c07748a5313fffb50
mrnithesh <mr.nithesh.k@gmail.com>
2025-01-24 18:40:09 +0530

    java commit 8

--- JAVA/prac2.java
+++ JAVA/prac2.java
@@ -3,13 +3,32 @@ import java.util.Scanner;
 public class prac2{
     public static void main(String [] args){
         Scanner s= new Scanner(System.in);
+        System.out.println("Enter A: ");
         int a = s.nextInt();
-        if ((a%400==0)|| (a%4==0 && a%100!=0)){
-            System.out.println("Leap year");
+        System.out.println("Enter B :");
+        int b = s.nextInt();
+        System.out.println("Enter C :");
+        int c = s.nextInt();
+
+        if ((a>=b) && (a>=c)){
+            System.out.println("A is greater");
+        }
+        else if ((b>=a) && (b>=c)){
+            System.out.println("B is greater");
         }
         else {
-            System.out.println("Non Leap year");
+            System.out.println("C is greater");
         }
-        s.close();
+
+
+
+
+        // if ((a%400==0)|| (a%4==0 && a%100!=0)){
+        //     System.out.println("Leap year");
+        // }
+        // else {
+        //     System.out.println("Non Leap year");
+        // }
+        // s.close();
     }
 }
