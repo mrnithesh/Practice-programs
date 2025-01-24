409582cd4c58e0bb2e6f17e180d07425e4a39942
mrnithesh <mr.nithesh.k@gmail.com>
2025-01-24 20:20:54 +0530

    java commit 11

--- JAVA/prac3.java
+++ JAVA/prac3.java
@@ -2,21 +2,25 @@ import java.util.Scanner;
 
 public class prac3{
     public static void main(String [] args){
-        // for (int i = 0; i <= 100; i++){
-        //     System.out.println(i);
-        // }
+        int result = 0;
+        for (int i = 0; i <= 100; i++){
+            if (i%2==0){
+                result+=i;
+            }
+        }
+        System.out.println(result);
         // int i=0;
         // while (i<=100){
         //     System.out.println(i);
         //     i++;
         // }
-        Scanner s= new Scanner(System.in);
-        System.out.println("Enter a number");
-        int a= s.nextInt();
+        // Scanner s= new Scanner(System.in);
+        // System.out.println("Enter a number");
+        // int a= s.nextInt();
         // for (int i=0; i<=20;i++){
         //     System.out.println(i +"*"+a + "=" + (i*a));
         // }
 
-        
+
     }
 }
