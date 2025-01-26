e67d062e53b5a30b73b97ea20d6d851c315869c5
mrnithesh <mr.nithesh.k@gmail.com>
2025-01-26 19:10:25 +0530

    java commit 14

--- JAVA/prac4.java
+++ JAVA/prac4.java
@@ -1,9 +1,18 @@
 import java.util.*;
 public class prac4{
     public static void main(String [] args){
-        int arr[] = {1,2,3,4,5};
-        for (int i=0;i<+arr.length;i++){
-            System.out.println(arr[i]);
+        int arr[] = {1,6,3,4,5};
+        int sum=0;
+        for (int i=0;i<arr.length;i++){
+            sum+=arr[i];
         }
+        System.out.println("sum ="+ sum);
+        int largest=0;
+        for (int i = 0; i<arr.length;i++){
+            if (arr[largest]<arr[i]){
+                largest=i;
+            }
+        }
+        System.out.println("The largest element is :"+ arr[largest]);        
     }
 }
