4004f750791bd7c14eeef2a113ba2fbbecc3b2cb
mrnithesh <mr.nithesh.k@gmail.com>
2025-02-02 16:40:19 +0530

    python ps-4

--- Python/gcd.py
+++ Python/gcd.py
@@ -1,8 +1,18 @@
 def find_gcd(a,b):
     gcd=1
+    #brute force
     for i in range(1,min(a,b)+1):
         if (a%i==0) and (b%i==0):
             gcd=i
     return gcd
 
+def gcd(a,b):
+    #recursion
+    if b==0:
+        return a
+    return gcd(b,a%b)
+
+
+
 print(find_gcd(120,18))
+print(gcd(18,120))
