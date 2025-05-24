dca08a57679281369985d08566d9a12378e20871
mrnithesh <mr.nithesh.k@gmail.com>
2025-05-24 17:28:37 +0530

    simple chatbot

---
+++ Python/AI/simplechatbot.py
@@ -0,0 +1,43 @@
+import base64
+import os
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+load_dotenv()
+
+def generate(user_input):
+    client = genai.Client(
+        api_key=os.environ.get("GEMINI_API_KEY"),
+    )
+
+    model = "gemini-2.0-flash"
+    contents = [
+        types.Content(
+            role="user",
+            parts=[
+                types.Part.from_text(text=user_input),
+            ],
+        ),
+    ]
+    generate_content_config = types.GenerateContentConfig(
+        temperature=1,
+        top_p=0.95,
+        top_k=40,
+        max_output_tokens=8192,
+        response_mime_type="text/plain",
+        system_instruction=[
+            types.Part.from_text(text="""always address the user as sir."""),
+        ],
+    )
+
+    for chunk in client.models.generate_content_stream(
+        model=model,
+        contents=contents,
+        config=generate_content_config,
+    ):
+        print(chunk.text, end="")
+
+if __name__ == "__main__":
+    while True:
+        text=input()
+        generate(text)
