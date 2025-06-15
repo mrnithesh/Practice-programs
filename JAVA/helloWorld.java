63aea5cf00b3b959250c0beec2b1b3a1dfad24d9
mrnithesh <mr.nithesh.k@gmail.com>
2025-06-15 07:30:00 +0530

    java fs #2

--- JAVA/helloWorld.java
+++ JAVA/helloWorld.java
@@ -1,5 +1,12 @@
 public class helloWorld{
     public static void main(String args[]){
         System.out.println("Hello World!!");
+        int a = 10;
+        float b = 10.00f;
+        char c = 'A';
+        double d = 3.14;
+        String e = "Something";
+        boolean isJavaFun = false;
+        
     }
 }
